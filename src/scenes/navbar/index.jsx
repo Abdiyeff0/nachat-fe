8dcac70f8ebf4f2e2bd61c150163766d6404ac14
Abdiyeff0 <g.abdiyef@gmail.com>
2023-11-19 17:26:13 +0400

    changes

--- src/scenes/navbar/index.jsx
+++ src/scenes/navbar/index.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
             },
           }}
         >
-          Sociopedia
+          NaChat
         </Typography>
         {isNonMobileScreens && (
           <FlexBetween
